3315e21361074987e52726e52011b8ee597fdce0
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2012-01-19 17:18:51 +0100

    Misc minor fixes.

--- opm_site/settings.py
+++ opm_site/settings.py
@@ -9,7 +9,7 @@
 ##
 ## This way, machine-specific settings override project-level settings.
 ##
-## A common naming scheme for these overlay settings files is as follows:
+## A common naming scheme for these "overlay" settings files is as follows:
 ##
 ## * ``local_settings.py`` -- for development machines
 ## * ``settings_staging.py`` -- for staging servers
--- opm_site/views.py
+++ opm_site/views.py
@@ -9,13 +9,12 @@ class InfoView(TemplateView):
     template_name = "info.html"
     
 class InstitutionDetailView(DetailView):
-
     context_object_name = "institution"
     model = Institution
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(InstitutionDetailView, self).get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
-        context['institutions_list'] = Institution.objects.all()
+        # Add in a QuerySet of all the institutions
+        context['institution_list'] = Institution.objects.all()
         return context
