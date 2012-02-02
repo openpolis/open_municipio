58db8d6dee66a92bc7d577c0a2910615b9fb5806
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2012-02-02 18:40:57 +0100

    Improved ``Category`` model.

--- open_municipio/taxonomy/models.py
+++ open_municipio/taxonomy/models.py
@@ -26,22 +26,50 @@ class TaggedItem(GenericTaggedItemBase, TaggedItemBase):
 
 class Category(models.Model):
     """
-    A label that can be used to categorize application objects.
+    A label that can be used to categorize content objects.
     
     Categories differ from tags in that the former are pre-defined at the application level,
     while tags are assigned (and created, if not already existing) on a per-instance basis. 
     """
-    name = models.CharField(max_length=128)
-    slug = models.SlugField()
-    tag_set = models.ManyToManyField(Tag, related_name='category_set')
+    name = models.CharField(verbose_name=_('Name'), max_length=100)
+    slug = models.SlugField(verbose_name=_('Slug'), blank=True, unique=True, max_length=100)
+    tag_set = models.ManyToManyField(Tag, related_name='category_set', null=True, blank=True)
   
-    def __unicode__(self):
-        return u'%s' % self.name
-    
     class Meta:
         verbose_name = _('category')
         verbose_name_plural = _('categories')
+        
+    def __unicode__(self):
+        return u'%s' % self.name
+    
+    def slugify(self, tag, i=None):
+        slug = slugify(tag)
+        if i is not None:
+            slug += "_%d" % i
+        return slug
+     
+    def calculate_slug(self):
+        """
+        Calculate a slug for a given category name.
+        
+        If a calculated slug already exists in the DB, add a numerical prefix until is OK.
+        """
+        slug = self.slugify(self.name)
+        i = 0
+        while True:
+            i += 1
+            try:
+                Category.objects.get(slug=slug)
+                slug = self.slugify(self.name, i)
+            except Category.DoesNotExist:
+                return slug
 
+    def save(self, *args, **kwargs):
+        # auto-generate a slug, if needed 
+        if not self.pk and not self.slug:
+            self.slug = self.calculate_slug()
+            return super(Category, self).save(*args, **kwargs)
+        
     @property
     def tags(self):
         return self.tag_set.all()
