2df005b863a156af4b4279f512873853faaffe06
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2012-02-02 18:38:49 +0100

    Added a custom ``TaggedItem`` model with additional metadata.

--- open_municipio/taxonomy/models.py
+++ open_municipio/taxonomy/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from taggit.models import Tag
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
+from taggit.models import Tag, GenericTaggedItemBase, TaggedItemBase
+
+class TaggedItem(GenericTaggedItemBase, TaggedItemBase):
+    """
+    A intermediate model to record associations between tags and content model instances.
+    
+    It's a `custom version`_ of ``taggit.models.TaggedItem``, adding a few tagging metadata:
+    
+    * ``tagger``: the user (an ``auth.User`` instance)  who tagged the item
+    * ``tagging_time``: a timestamp specifying when an item was tagged
+    
+    .. _`custom version`: http://readthedocs.org/docs/django-taggit/en/latest/custom_tagging.html
+    """
+    tagger = models.ForeignKey(User, null=True, blank=True, editable=False)
+    tagging_time = models.DateTimeField(null=True, auto_now_add=True)
+    
+    class Meta:
+        verbose_name = _("tagged item")
+        verbose_name_plural = _("tagged items")
 
 
 class Category(models.Model):
