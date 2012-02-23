0197c32e83ecb1ab318544c10a95dcdc3052777e
Fabio Natali <fabio@fnstudio.(none)>
2012-02-23 21:37:29 +0100

    A few fixes to previous commit/push

--- open_municipio/acts/views.py
+++ open_municipio/acts/views.py
@@ -1,1 +1,14 @@
+from django.views.generic import DetailView, TemplateView
+from open_municipio.acts.models import Act
 
+
+class ActDetailView(DetailView):
+    context_object_name = "act"
+    model = Act
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ActDetailView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the institutions
+        context['request'] = self.request
+        return context
--- open_municipio/om_comments/admin.py
+++ open_municipio/om_comments/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin.sites import NotRegistered
 from django.contrib.comments.admin import CommentsAdmin
 
 from open_municipio.om_comments.models import CommentWithMood
@@ -6,14 +7,13 @@ from open_municipio.om_comments.models import CommentWithMood
 class CommentWithMoodAdmin(CommentsAdmin):
     pass
 
-# A try/except wrapper is necessary here to prevent this class to be
-# unregistered more than once, which would lead to errors. Those
+# A try/except wrapper is necessary here to prevent this class from
+# being unregistered more than once, which would lead to errors. Those
 # multiple unregister attempts are probably due to circular imports
 # (``from django.contrib.comments.admin import CommentsAdmin``).
 try:
     admin.site.unregister(CommentWithMood)
-# FIXME: be more specific here (try without the wrapper and see what kind of error is thrown exactly)
-except:
+except NotRegistered:
     pass
 
 admin.site.register(CommentWithMood, CommentWithMoodAdmin)
