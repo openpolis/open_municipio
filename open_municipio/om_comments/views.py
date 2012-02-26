a56dad8b36c65864796dee766b61dff0c34f11bc
Fabio Natali <fabio@fnstudio.(none)>
2012-02-26 13:38:11 +0100

    Comment removal functionality
    Users can delete their own comments within a certain time
    lapse. Template must show delete buttons only when removal is
    possible. A custom template filter is provided to address this. The
    filter returns the number of seconds that a certain comment is still
    available for removal.

--- open_municipio/om_comments/views.py
+++ open_municipio/om_comments/views.py
@@ -1,3 +1,4 @@
+from django import template
 from django.conf import settings
 from django.contrib import comments
 from django.contrib.auth.decorators import login_required
@@ -8,20 +9,29 @@ from django.shortcuts import get_object_or_404, redirect
 import datetime
 
 
-# Max time within users can delete their own comments
-MAX_TIME_FOR_COMMENT_REMOVAL = datetime.timedelta(minutes=10)
+# Number of minutes within users can delete their own comments
+MAX_TIME_FOR_COMMENT_REMOVAL = 10
 
 @login_required
 def delete_own_comment(request, message_id):
-    comment = get_object_or_404(comments.get_model(), pk=message_id,
-                                site__pk=settings.SITE_ID)
+    """
+    Users can delete their own comments within a certain time lapse.
+    """
 
-    submit_date = comment.submit_date
     now = datetime.datetime.now()
-    time_elapsed = now - submit_date
+    control_date = now - datetime.timedelta(minutes=MAX_TIME_FOR_COMMENT_REMOVAL)
 
-    if (time_elapsed < MAX_TIME_FOR_COMMENT_REMOVAL and comment.user == request.user):
+    # I retrieve the comment if it's been written in the past few
+    # minutes: ``submit_date__gte=control_date``
+    comment = get_object_or_404(comments.get_model(), pk=message_id,
+                                site__pk=settings.SITE_ID,
+                                submit_date__gte=control_date)
+
+    # Does current user really own it?
+    if (comment.user == request.user):
+        # Then flag it as removed!
         comment.is_removed = True
         comment.save()
 
+    # Whatever happened, just get back to the act page
     return redirect('act-detail', comment.content_object.id)
