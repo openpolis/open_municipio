7f5dc9483201360d5a92e824e4ea899f56b84fa9
Guglielmo <guglielmo.celata@gmail.com>
2012-03-02 15:53:14 +0100

    samples and documentation added to schema xml

--- open_municipio/settings.py
+++ open_municipio/settings.py
@@ -21,20 +21,6 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 REPO_ROOT = os.path.abspath(os.path.dirname(PROJECT_ROOT))
 VERSION = __version__ = file(os.path.join(PROJECT_ROOT, 'VERSION')).read().strip()
 
-INTERNAL_IPS = ('127.0.0.1',)
-
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-)
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -133,7 +119,7 @@ INSTALLED_APPS = (
     'open_municipio.people',
     'open_municipio.taxonomy',
     'open_municipio.votations',
-#   'debug_toolbar',
+    'open_municipio.newscache',
 )
 
 COMMENTS_APP = 'open_municipio.om_comments'
