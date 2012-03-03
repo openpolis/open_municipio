3f2a24d96f385899d5801fe678662e76fa4b81e7
fspegni <francesco.spegni@gmail.com>
2012-03-03 10:30:23 +0100

    synchronized with dev

--- open_municipio/settings.py
+++ open_municipio/settings.py
@@ -14,15 +14,11 @@
 ## * ``settings_local.py`` -- for development machines
 ## * ``settings_staging.py`` -- for staging servers
 ## * ``settings_production.py`` -- for production servers
-
 import os
-
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 REPO_ROOT = os.path.abspath(os.path.dirname(PROJECT_ROOT))
 VERSION = __version__ = file(os.path.join(PROJECT_ROOT, 'VERSION')).read().strip()
-
 INTERNAL_IPS = ('127.0.0.1',)
-
 DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.version.VersionDebugPanel',
     'debug_toolbar.panels.timer.TimerDebugPanel',
@@ -34,16 +30,12 @@ DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.signals.SignalDebugPanel',
     'debug_toolbar.panels.logger.LoggingPanel',
 )
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
-
 MANAGERS = ADMINS
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -52,21 +44,16 @@ MANAGERS = ADMINS
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
 TIME_ZONE = 'Europe/Rome'
-
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'it-IT'
-
 SITE_ID = 1
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
-
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
 USE_L10N = True
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
@@ -74,19 +61,15 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
-
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
 )
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -95,15 +78,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
-
-
 TEMPLATE_DIRS = (
   os.path.join(PROJECT_ROOT, 'templates'),
-  os.path.join(PROJECT_ROOT, 'templates/om'),
-  os.path.join(PROJECT_ROOT, 'templates/admin'),
-  os.path.join(PROJECT_ROOT, 'templates/static'),
 )
-
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
@@ -113,7 +90,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages",
     "django.core.context_processors.request",
 )
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.comments',
@@ -126,7 +102,7 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'django.contrib.staticfiles',
     'django_extensions',
-    'south',
+    #'south',
     'taggit',
     'voting',
     'open_municipio.inline_edit',
@@ -138,5 +114,5 @@ INSTALLED_APPS = (
     'open_municipio.votations',
 #   'debug_toolbar',
 )
-
 COMMENTS_APP = 'open_municipio.om_comments'
+
