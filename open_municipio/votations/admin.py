575b2c8610615cbe1668be9ebfb28dab27f44244
Guglielmo <guglielmo.celata@gmail.com>
2012-03-10 00:36:08 +0100

    the votation admin interface, whenever entered as a staf user which is not superuser, allows the linking of an act to a votation, through a raw field lookup

--- open_municipio/votations/admin.py
+++ open_municipio/votations/admin.py
@@ -1,4 +1,5 @@
-from django.contrib import admin 
+from httplib import HTTP
+from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
 from open_municipio.votations.models import ChargeVote, GroupVote, Votation  
@@ -13,17 +14,36 @@ class ChargeVoteInline(admin.TabularInline):
     model = ChargeVote
     raw_id_fields = ('charge', )
     extra = 1
- 
-class VotationAdminWithGroupsAndChargesVotes(admin.ModelAdmin):
+
+class VotationAdmin(admin.ModelAdmin):
     list_display = ('idnum', 'act_descr', 'sitting', 'is_linked', 'outcome')
     list_filter = ('act', 'sitting',)
+    raw_id_fields = ['act']
     readonly_fields = ('act_descr', )
-    inlines = [GroupVoteInline, ChargeVoteInline] 
+    ordering = ['-sitting__date']
+
+
+
+    # add inlines only for superuser users
+    def change_view(self, request, object_id, extra_context=None):
+
+        if request.user.is_superuser:
+            self.inlines = [GroupVoteInline, ChargeVoteInline]
+        else:
+            self.inlines = []
+
+        self.inline_instances = []
+        for inline_class in self.inlines:
+            inline_instance = inline_class(self.model, self.admin_site)
+            self.inline_instances.append(inline_instance)
+
+        return super(VotationAdmin, self).change_view(request, object_id)
+
 
 class VotationsInline(admin.TabularInline):
     model = Votation
     fields = ('n_legal', 'n_presents', 'n_maj', 'n_yes', 'n_no', 'n_abst', 'outcome')
-    
-admin.site.register(Votation, VotationAdminWithGroupsAndChargesVotes)
+
+admin.site.register(Votation, VotationAdmin)
 admin.site.register(GroupVote)
 admin.site.register(ChargeVote)
