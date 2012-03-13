fb76dfba5295586e09f8b6a1d77da56eebb39bda
Daniele <joke2k@gmail.com>
2012-03-13 16:19:21 +0100

    Person and Citizen templates

--- open_municipio/people/urls/people.py
+++ open_municipio/people/urls/people.py
@@ -1,17 +1,15 @@
 from django.conf.urls.defaults import *
 from django.views.generic import DetailView
+from django.views.generic.list import ListView
 
 from open_municipio.people.models import Person
 
 
 urlpatterns = patterns('',
-    url(r'^citizen/(?P<slug>[-\w]+)/$', DetailView.as_view(
-        model=Person,
-        context_object_name='person',
-        template_name='people/citizen_detail.html',
-    ), name='om_citizen_detail'),
 
-    url(r'^person/(?P<slug>[-\w]+)/$', DetailView.as_view(
+    url(r'^$', ListView.as_view(template_name='people/person_list.html', model=Person), name='om_politician_list'),
+
+    url(r'^(?P<slug>[-\w]+)/$', DetailView.as_view(
         model=Person,
         context_object_name='person',
         template_name='people/person_detail.html',
