ef7e7eff08e61059813efb2fb361656e2ae33736
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2012-03-19 13:09:29 +0100

    Minor cleanups for the ``monitoring`` app.
    * docstrings tweaking
    * within  URL patterns:
    
      * replaced un-named groups with named ones
      * adjusted view names in order to comply with dev guidelines
    * made some annotations

--- open_municipio/monitoring/views.py
+++ open_municipio/monitoring/views.py
@@ -2,27 +2,36 @@ from django.http import HttpResponseRedirect
 
 from open_municipio.monitoring.forms import MonitoringForm
 
-from os import sys
-
+# FIXME: convert to a CBV
 def start(request):
     """
-    starts a monitoring
-    user_id and content to monitor are taken from request.POST data
-    redirects to referer after action
+    Start the monitoring process of a content object by a given user.
+    
+    Monitoring user and target content object are taken from POST data.
+    
+    Redirect to referrer URL after processing.
     """
     if request.method == 'POST':
         form = MonitoringForm(data=request.POST)
-        if form.is_valid():
-            monitoring = form.save()
+        if form.is_valid():  form.save()
+    # FIXME: redirects shouldn't rely on the ``Referer`` header,
+    # since it might be missing from the HTTP request,
+    # depending on client's configuration.  
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
+# FIXME: convert to a CBV
 def stop(request):
     """
-    stops a monitoring, by removing it 
-    redirects to referer after action    
+    Stop the monitoring process of a content object by a given user.
+    
+    Monitoring user and target content object are taken from POST data.
+    
+    Redirect to referrer URL after processing.    
     """
     if request.method == 'POST':
         form = MonitoringForm(data=request.POST)
-        if form.is_valid():
-            form.remove()
+        if form.is_valid():  form.remove()
+    # FIXME: redirects shouldn't rely on the ``Referer`` header,
+    # since it might be missing from the HTTP request,
+    # depending on client's configuration.  
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
