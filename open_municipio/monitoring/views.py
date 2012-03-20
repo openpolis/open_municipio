ab507baacaf752285fb80996ecabdb1e9ed90149
Guglielmo <guglielmo.celata@gmail.com>
2012-03-20 16:08:07 +0100

    user that starts or stops a monitoring is retrieved from the session (currently logged user) closes #46; users with no profiles cannot start monitoring an object: closes #45

--- open_municipio/monitoring/views.py
+++ open_municipio/monitoring/views.py
@@ -1,8 +1,11 @@
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect
 
 from open_municipio.monitoring.forms import MonitoringForm
 
 from os import sys
+from open_municipio.monitoring.models import Monitoring
 
 def start(request):
     """
@@ -10,10 +13,30 @@ def start(request):
     user_id and content to monitor are taken from request.POST data
     redirects to referer after action
     """
-    if request.method == 'POST':
-        form = MonitoringForm(data=request.POST)
-        if form.is_valid():
-            monitoring = form.save()
+
+    # chek if current user has a profile
+    try:
+        current_user_profile = request.user.get_profile()
+    except ObjectDoesNotExist:
+        current_user_profile = False
+
+    # starts a monitoring, by creating an instance,
+    # only if non-existing
+    if request.method == 'POST' and current_user_profile:
+        try:
+            monitoring = Monitoring.objects.get(
+                content_type=ContentType.objects.get(pk=request.POST['content_type_id']),
+                object_pk=request.POST['object_pk'],
+                user=request.user
+            )
+        except ObjectDoesNotExist:
+            monitoring = Monitoring(
+                content_type_id=request.POST['content_type_id'],
+                object_pk=request.POST['object_pk'],
+                user=request.user
+            )
+            form = MonitoringForm(request.POST, instance=monitoring)
+            form.save()
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 def stop(request):
@@ -21,8 +44,19 @@ def stop(request):
     stops a monitoring, by removing it 
     redirects to referer after action    
     """
-    if request.method == 'POST':
-        form = MonitoringForm(data=request.POST)
+    # chek if current user has a profile
+    try:
+        current_user_profile = request.user.get_profile()
+    except ObjectDoesNotExist:
+        current_user_profile = False
+
+    if request.method == 'POST' and current_user_profile:
+        monitoring = Monitoring.objects.get(
+            content_type=ContentType.objects.get(pk=request.POST['content_type_id']),
+            object_pk=request.POST['object_pk'],
+            user=request.user
+        )
+        form = MonitoringForm(request.POST, instance=monitoring)
         if form.is_valid():
-            form.remove()
+            monitoring.delete()
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
