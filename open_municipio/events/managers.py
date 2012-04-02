e6d331b5579e15fdacc20d7298ed6aa76ddcec49
Fabio Natali <fabio@fnstudio.(none)>
2012-04-02 17:09:17 +0200

    All this manager stuff is no longer necessary

--- open_municipio/events/managers.py
+++ open_municipio/events/managers.py
@@ -15,36 +15,3 @@ class EventManager(models.Manager):
         now = datetime.now()
         start_date = now - timedelta(days=EVENTS_DAYS_IN_THE_PAST)
         return super(EventManager, self).get_query_set().filter(date__gt=start_date)
-
-    def get_by_act(self, act):
-        """
-        Returns events regarding a specific act
-        """
-        return self.get_query_set().filter(act__pk=act.pk)
-        
-    def get_by_institution(self, institution):
-        """
-        Returns events regarding a specific institution
-        """
-        return self.get_query_set().filter(institution__pk=institution.pk)
-        
-    #FIXME: this is just a stub
-    def get_by_category(self, category):
-        """
-        Returns events regarding acts under a specific category
-        """
-        return self.get_query_set().filter(act__category=category)
-        
-    #FIXME: this is just a stub
-    def get_by_tag(self, tag):
-        """
-        Returns events regarding acts under a specific tag
-        """
-        return self.get_query_set().filter(act__tag=tag)
-        
-    #FIXME: this is just a stub
-    def get_by_politician(self, politician):
-        """
-        Returns events regarding acts signed by a specific politician
-        """
-        return self.get_query_set().filter(act__politician=politician)
