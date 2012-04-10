1dbda612c7f94a43113f0bfce8eb2d49e85e1418
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2012-04-10 14:50:10 +0200

    Fixed a typo.

--- open_municipio/monitoring/forms.py
+++ open_municipio/monitoring/forms.py
@@ -7,7 +7,7 @@ class MonitoringForm(ModelForm):
     """
     A form to start or stop monitoring an object.
     
-    It contains three hidden fields, that must take value when the form is created.
+    It contains two hidden fields, that must take value when the form is created.
     """
     content_type_id = forms.IntegerField(widget=forms.HiddenInput)
     object_pk = forms.IntegerField(widget=forms.HiddenInput)
