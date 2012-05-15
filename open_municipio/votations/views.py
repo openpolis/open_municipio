506152af5353dfc76a293314d426f268c7c9ca92
Guglielmo <guglielmo.celata@gmail.com>
2012-05-15 19:04:02 +0200

    VotationDetailView refactored (simplification)

--- open_municipio/votations/views.py
+++ open_municipio/votations/views.py
@@ -55,34 +55,4 @@ class VotationDetailView(DetailView):
     """
     model = Votation
     template_name = 'votations/votation_detail.html'
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super(VotationDetailView, self).get_context_data(**kwargs)
-
-        votation = self.object
-
-        if votation.act:
-            # So we have an act. What kind of act, specifically? (FIXME: I
-            # don't like the way act types are hardcoded here with their
-            # Italian names.)
-            if votation.act.downcast().__class__() == Agenda().__class__():
-                act_type = "Ordine del Giorno"
-            elif votation.act.downcast().__class__() == Deliberation().__class__():
-                act_type = "Delibera"
-            elif votation.act.downcast().__class__() == Interpellation().__class__():
-                act_type = "Interpellanza"
-            elif votation.act.downcast().__class__() == Interrogation().__class__():
-                act_type = "Interrogazione"
-            elif votation.act.downcast().__class__() == Motion().__class__():
-                act_type = "Mozione"
-
-            votation.act_type = act_type
-
-        extra_context = {
-            'votation': votation,
-            }
-
-        # Update context with extra values we need
-        context.update(extra_context)
-        return context
+    context_object_name = 'votation'
