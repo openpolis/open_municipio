d5b1efc48e6bc5b6689d4817f4fb2bfdd3b8f60b
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2012-05-22 14:10:22 +0200

    First draft implementation of ``MdbVotationReader`` class.

--- data_import/municipalities/senigallia/lib.py
+++ data_import/municipalities/senigallia/lib.py
@@ -2,7 +2,8 @@ from data_import.utils import create_table_schema, get_row_dicts
 from data_import.votations.lib import Ballot, Sitting, Vote, BaseVotationReader, VotationDataSource
 from data_import.municipalities.senigallia import conf
 
-import re, os, sqlite3
+import re, os, sqlite3, datetime, logging, struct
+
 
 
 class MdbDataSource(VotationDataSource):
@@ -92,18 +93,20 @@ class MdbDataSource(VotationDataSource):
                                } 
         
         for (table_name, table_schema) in tables.items():
-            cursor.execute('DROP TABLE IF EXISTS %s;' % table_name)
+            cursor.execute('DROP TABLE IF EXISTS %s' % table_name)
             create_query = create_table_schema(table_name, table_schema)
             cursor.execute(create_query)
         ## Transcoding from MDBs to SQLite
         for table_name in tables:
             ## Export data records from MDBs
-            export_command = "mdb-export -I %(fname)s %(table_name)s | sed -e 's/)$/)\;/'"
+            export_command = "mdb-export -I %(fname)s %(table_name)s"
             export_output = os.popen(export_command % {'fname': mdb_fpath, 'table_name': table_name}).read()
             insert_queries = [line for line in export_output.split('\n') if line.startswith('INSERT')]
             for insert_query in insert_queries:
                 ## Import data records into SQLite
-                cursor.execute(insert_query)    
+                cursor.execute(insert_query)
+        ## commit DB changes
+        connection.commit()
         ## close DB connection    
         connection.close()       
     
@@ -133,13 +136,130 @@ class MdbDataSource(VotationDataSource):
         sittings = []
         # loop over the SQLite DBs containing votation data
         for fname in os.listdir(conf.SQLITE_ROOT_DIR):
+            logging.info("Processing %s" % os.path.join(conf.SQLITE_ROOT_DIR, fname))
             # sitting IDs are encoded within DB filenames
             sitting_id = fname.rstrip('.sqlite')
             sitting = Sitting()
+            sitting._id = sitting_id # internal use only
             sitting.seq_n = self._get_sitting_seq_n(sitting_id)
-            # TODO: retrieve sitting's ``call`` and ``date`` attributes, and set them
-            sittings.append(sitting)  
-    
+            # make a connection to the SQLite DB
+            db_file_path = os.path.join(conf.SQLITE_ROOT_DIR, fname)
+            connection = sqlite3.connect(db_file_path)
+            cursor = connection.cursor()
+            ## Retrieve sitting info
+            query = 'SELECT DataSeduta, Convocazione FROM CostSed'
+            cursor.execute(query)
+            results = cursor.fetchall()
+            connection.close()
+            # sanity check
+            if len(results) != 1:
+                logging.error("Corrupted table `CostSed` for sitting #%s" % sitting_id)
+            row = results[0]
+            # sitting's date, as returned by the DB (i.e. a string)
+            date_string = row[0]
+            # convert to a Python ``Date`` object
+            sitting.date =  datetime.datetime.strptime(date_string, '%m/%d/%y %H:%M:%S').date()
+            sitting.call = row[1]
+            sittings.append(sitting)           
+        return sittings  
+        
+    def get_ballots(self, sitting):
+        BALLOT_TYPES = {
+                        1: 'Palese Semplice',
+                        2: 'Palese Nominale',
+                        3: 'Numero Legale',
+                        4: 'Segreta',                            
+                        }
+        OUTCOMES = {
+                    0: 'No Esito',
+                    1: 'Approvato',
+                    2: 'Respinto',
+                    3: 'SI Numero Legale',
+                    4: 'NO Numero Legale',
+                    5: 'Annullata',                                                                
+                    }
+        ballots = []
+        # DB to query against
+        db_file_path = os.path.join(conf.SQLITE_ROOT_DIR, sitting._id + '.sqlite')            
+        connection = sqlite3.connect(db_file_path)
+        cursor = connection.cursor()
+        query = 'SELECT * FROM Votazioni'
+        row_dicts = get_row_dicts(cursor, query)
+        for row_dict in row_dicts:
+            # TODO: filter out irrelevant ballots
+            ballot = Ballot(
+                sitting = sitting,
+                seq_n = row_dict['NumVoto'],
+                timestamp = datetime.datetime.strptime(row_dict['Data_Ora '], '%m/%d/%y %H:%M:%S'),
+                ballot_type = BALLOT_TYPES[row_dict['TipoVoto']],
+                n_presents = row_dict['Presenti'],
+                n_partecipants = row_dict['Votanti'],
+                n_yes = row_dict['Favorevoli'],
+                n_no = row_dict['Contrari'],
+                n_abst = row_dict['Astenuti'],
+                n_legal = row_dict['NumLegale '],
+                outcome = OUTCOMES[row_dict['Esito']],
+                )
+            ballots.append(ballot)
+        return ballots
+                     
+    def get_votes(self, ballot):
+        VOTE_OUTCOME = {
+                      'FAV': 'Favorevole',
+                      'CON': 'Contrario',
+                      'AST': 'Astenuto', 
+                      'VOT': 'Votante (Vot. Segreta)',
+                      'NVT': 'Non Votante (4 Tasto)',
+                      'PRE': 'Presente(Vot N.L.)',
+                      '...': 'Tessera Presente Non Votante (Se presente un numero di tessera)',
+                      '___': 'Terminale NON INSTALLATO',
+                      'ECP': 'Tessera Capovolta',
+                      'ETP': 'Errore tipo Tessera',
+                      'ELE': 'Errore lettura Tessera',
+                      'ELG': 'Errata Legislatura',
+                      'EFW': 'Errore Release firmware', 
+                      'ENR': 'Errore Non Risponde',
+                      'BDO': 'Errore BDO',
+                      'EAB': 'Errore Tessera non abilitata',
+                      'EPO': 'Errore Posto (posti fissi)',
+                      }           
+        votes = []
+        db_file_path = os.path.join(conf.SQLITE_ROOT_DIR, ballot.sitting._id + '.sqlite')
+        connection = sqlite3.connect(db_file_path)
+        cursor = connection.cursor()
+        ## retrieve individual counselors' votes for the given ballot
+        query = 'SELECT Dettaglio FROM Votazioni WHERE NumVoto = ?'
+        cursor.execute(query, [ballot.seq_n])
+        results = cursor.fetchall()
+        connection.close()
+        # sanity check
+        if len(results) != 1:
+            logging.error("Corrupted table `Votazioni` for sitting #%s" % ballot.sitting._id)
+        # individual votes are encoded within a binary (but ASCII-only) string
+        ballot_detail_str = results[0][0]
+        # sanity check: string encoding votes detail must be a multiple of 14
+        # (since it's made of 14-bytes records)
+        if len(ballot_detail_str) % 14 != 0:
+            logging.error("Corrupted table `Votazioni` for sitting #%s" % ballot.sitting._id)
+        records = [ballot_detail_str[i:i+14] for i in range(0, len(ballot_detail_str), 14)]
+        for record in records:
+            ## record format: ``CCCCGGGGTTTXXX``, where:
+            ## ``CCCC``: component ID
+            ## ``GGGG``: group ID
+            ## ``TTT``: card ID
+            ## ``XXX``: issued vote
+            componentID, groupID, cardID, vote_code = struct.unpack('4s4s3s3s', record)
+            vote = Vote(
+                        ballot = ballot,
+                        cardID = cardID, 
+                        componentID = componentID, 
+                        groupID = groupID, 
+                        choice = VOTE_OUTCOME[vote_code]
+                        )           
+            # TODO: filter out irrelevant votes               
+            votes.append(vote)
+        return votes
+
 
 class MdbVotationReader(BaseVotationReader):
     """
