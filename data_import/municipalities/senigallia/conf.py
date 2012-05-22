34146406333a302c17d20fc01c2619f6723d89f9
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2012-05-22 14:09:36 +0200

    Updated conf settings.

--- data_import/municipalities/senigallia/conf.py
+++ data_import/municipalities/senigallia/conf.py
@@ -1,6 +1,6 @@
 # where MDB files are located on the filesystem
-MDB_ROOT_DIR = '/home/lorenzo/djcode/open_municipio/data_import/senigallia/test_data/mdb'
+MDB_ROOT_DIR = '/home/lorenzo/djcode/open_municipio/data_import/municipalities/senigallia/test_data/mdb'
 # where SQLite files are located on the filesystem
-SQLITE_ROOT_DIR = '/home/lorenzo/djcode/open_municipio/data_import/senigallia/test_data/sqlite'
+SQLITE_ROOT_DIR = '/home/lorenzo/djcode/open_municipio/data_import/municipalities/senigallia/test_data/sqlite'
 # a regexp describing valid filenames for MDB files containing votation-related data
 MDB_FILENAME_PATTERN = r'SeniS(?P<sitting_id>\d{4})\.Mdb'
