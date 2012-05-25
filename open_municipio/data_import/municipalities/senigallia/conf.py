43c610cbed73a08f784a310e427d31246c5371ba
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2012-05-25 14:23:03 +0200

    Minor tweaks.

--- open_municipio/data_import/municipalities/senigallia/conf.py
+++ open_municipio/data_import/municipalities/senigallia/conf.py
@@ -7,4 +7,4 @@ MDB_ROOT_DIR = os.path.join(settings.PROJECT_ROOT, 'data_import/municipalities/s
 # where SQLite files are located on the filesystem
 SQLITE_ROOT_DIR = os.path.join(settings.PROJECT_ROOT, 'data_import/municipalities/senigallia/test_data/sqlite')
 # a regexp describing valid filenames for MDB files containing votation-related data
-MDB_FILENAME_PATTERN = r'SeniS(?P<sitting_id>\d{4})\.Mdb'
+MDB_SITTING_FNAME_PATTERN = r'SeniS(?P<sitting_id>\d{4})\.Mdb'
