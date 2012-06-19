d6ac41f86d992dc9787ae1fbbe8c190090ef4b59
fnstudio <fabio@fnstudio.it>
2012-06-19 16:15:32 +0200

    Ooops: profile data not properly saved at registration time

--- open_municipio/users/regbackend.py
+++ open_municipio/users/regbackend.py
@@ -3,13 +3,15 @@ from users.forms import UserRegistrationForm
 from users.models import UserProfile
 
 
-def user_created(sender, user, request, **kwargs):        
+def user_created(sender, user, request, **kwargs):
     form = UserRegistrationForm(request.POST)
     user.first_name = form.data['first_name']
     user.last_name = form.data['last_name']
     user.save()
     
     extra_data = UserProfile(user=user)
+    extra_data.says_is_politician = form.data['says_is_politician']
+    extra_data.uses_nickname = form.data['uses_nickname']
     extra_data.privacy_level = form.data['privacy_level']
     extra_data.wants_newsletter = False
     if 'wants_newsletter' in request.POST:
