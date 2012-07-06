27ab3b2d808ca88973eb113db8efb1fa19edb323
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2012-07-06 17:20:16 +0200

    Minor improvements to the ``TimeFramedQuerySet`` custom QuerySet.
    * renamed the ``moment`` argument to ``as_of``
    * ``current()`` now take a Python datetime object, not a string (handier, IMO)
    * minor implementation tweaks
    * added some annotations

--- open_municipio/people/managers.py
+++ open_municipio/people/managers.py
@@ -10,12 +10,14 @@ class TimeFramedQuerySet(QuerySet):
     
     Here, a *timeframed model* denotes a model class having an associated time range.
     
-    We assume that the time range is described by two ``Date`` (or ``DateTime``) fields
+    We assume that this time range is implemented as two ``Date`` (or ``DateTime``) fields
     named ``start_date`` and ``end_date``, respectively.
     """
+    # FIXME: this manager is rather generic, so it should be moved 
+    # under either the ``om`` or ``om_utils`` application
     def past(self):
         """
-        Return a QuerySet containing the *past* instances of the model
+        Return a queryset containing *past* instances of the model
         (i.e. those having an end date which is in the past).
         """
         now = datetime.now()
@@ -23,24 +25,20 @@ class TimeFramedQuerySet(QuerySet):
     
     def future(self):
         """
-        Return a QuerySet containing the *future* instances of the model
+        Return a queryset containing *future* instances of the model
         (i.e. those having a start date which is in the future).
         """
         now = datetime.now()
         return self.filter(start_date__gte=now)
 
-    def current(self, moment=None):
+    def current(self, as_of=None):
         """
-        Return a QuerySet containing the *current* instances of the model
-        at the given moment in time, if the parameter is spcified
-        now if it is not
-        @moment - is a datetime, expressed in the YYYY-MM-DD format
-        (i.e. those for which the moment date-time lies within their associated time range).
+        Return a queryset containing model instances which was *current* 
+        at a given point in time (as specified by the ``as_of`` argument).
+        
+        The ``as_of`` argument, if provided, should be a Python datetime object, 
+        defaulting to the current date/time.
         """
-        if moment is None:
-            moment = datetime.now()
-        else:
-            moment = datetime.strptime(moment, "%Y-%m-%d")
-
-        return self.filter(Q(start_date__lte=moment) &
-                           Q(end_date__gte=moment) | Q(end_date__isnull=True))
+        time = as_of or datetime.now()
+        return self.filter(Q(start_date__lte=time) &
+                           Q(end_date__gte=time) | Q(end_date__isnull=True))
