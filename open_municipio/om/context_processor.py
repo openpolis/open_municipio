ef7b821b4f1be18982ef8fa960596212060c3e48
Guglielmo <guglielmo.celata@gmail.com>
2012-09-11 19:11:40 +0200

    demo alerts are optionals, some more variables are set in the context_processor

--- open_municipio/om/context_processor.py
+++ open_municipio/om/context_processor.py
@@ -4,4 +4,11 @@ def defaults(request):
     '''
     A context processor to add the default site settings to the current Context
     '''
-    return settings.SITE_INFO
+    return {
+        'main_city': settings.SITE_INFO['main_city'],
+        'beta': settings.SITE_INFO['site_version'],
+        'DEBUG': settings.DEBUG,
+        'TEMPLATE_DEBUG': settings.TEMPLATE_DEBUG,
+        'STATIC_URL': settings.STATIC_URL,
+        'IS_DEMO': settings.IS_DEMO,
+    }
