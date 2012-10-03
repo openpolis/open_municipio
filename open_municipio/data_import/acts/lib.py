94dc436c5229cdc78f4ddee3bdd80d5110015c21
fspegni <francesco.spegni@gmail.com>
2012-10-04 00:38:17 +0200

    added intermediate level for imported acts

--- open_municipio/data_import/acts/lib.py
+++ open_municipio/data_import/acts/lib.py
@@ -5,6 +5,7 @@ from open_municipio.data_import.lib import DataSource, BaseReader, BaseWriter, J
 from open_municipio.data_import.om_xml import *
 
 from lxml import etree
+from mechanize._beautifulsoup import Null
 
 class ActsDataSource(DataSource):
     """
@@ -20,7 +21,20 @@ class BaseActsReader(BaseReader):
     It's intended to be subclassed (and its methods overriden) in order to adapt to 
     concrete scenarios. 
     """
-    pass
+    def read(self):
+        # initialize the reader
+        self.setup()
+        # get the data source to read from
+        data_source = self.get_data_source()
+        # initialize the data source
+        data_source.setup()
+        
+        self.acts = []
+        
+        # TODO extract the relevant information
+        
+        return self.acts;      
+
 
 class GenericActsReader(BaseActsReader):
     pass
@@ -42,4 +56,90 @@ class OMActsWriter(BaseActsWriter, OMWriter):
     def write(self):
         raise NotImplementedError
     
+# python object layer for imported data
+
+# document section
+
+class Act:
+    id = ""
+    content = ""
+    file = Null
+    subscribers = [] # list of Charges
+    
+    def add_subscriber(self, charge):
+        self.subscribers.append(charge)
+
+class CouncilDeliberation(Act):
+    final_id = ""
+    execution_date = ""
+    initiative = "" # (council_member, ...)
+
+class CityGovernmentDeliberation(Act):
+    pass
+
+class Interrogation(Act):
+    pass
+
+class Interpellation(Act):
+    pass
+
+class Motion(Act):
+    pass
+
+class Agenda(Act):
+    pass
+
+class Emendation(Act):
+    pass
+
+class Attachment:
+    description = ""
+    file = Null
+    type = "" # type of file (define an array of possible file types)
+    
+# people section
+
+class Person:
+    id = ""
+    name = ""
+    surname = ""
+    ssn = ""
+
+class Charge:
+    id = ""
+    start_date = ""
+    person = Null
+    name = Null
+    description = ""
+    
+#    def __init__(self, id, name, surname, ssn):
+#        new_person = Person()
+#        new_person.id = id
+#        new_person.name = name
+#        new_person.surname = surname
+#        new_person.ssn = ssn
+#        self.person = person
+    
+# institution section
+    
+class Institution:    
+    charges = []
+    
+    def add_charge(self, charge):
+        self.charges.append(charge)
+    
+class Mayor(Institution):
+    
+    def __init__(self, mayor):
+        self.append(mayor)
+
+class CityGovernment(Institution):
+    pass
+
+class CityCouncil(Institution):
+    pass
+    
+class Subscriber:
+    charge = Null
+    type = "" # first subscriber or co-subscriber
     
