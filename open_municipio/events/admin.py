9e0905c1d41e0b7cc653ca949ecbad2577cbf4e1
Guglielmo <guglielmo.celata@gmail.com>
2012-11-06 16:14:49 +0100

    event's description riche text editor configured optimally

--- open_municipio/events/admin.py
+++ open_municipio/events/admin.py
@@ -5,7 +5,20 @@ from tinymce.widgets import TinyMCE
 
 
 class EventForm(ModelForm):
-    description = CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 25}))
+    description = CharField(widget=TinyMCE(
+        attrs={'cols': 80, 'rows': 25},
+        mce_attrs={
+            'theme': "advanced",
+            'theme_advanced_buttons1': "formatselect,bold,italic,underline|,bullist,numlist,|,undo,redo,|,link,unlink,|,code,help",
+            'theme_advanced_buttons2': "",
+            'theme_advanced_buttons3': "",
+            'theme_advanced_blockformats': "p,blockquote",
+            'theme_advanced_resizing': True,
+            'theme_advanced_statusbar_location': "bottom",
+            'theme_advanced_toolbar_location': "top",
+            'theme_advanced_path': False
+        }
+    ))
     class Meta:
         model = Event
 
