2c536a7752e5fd85eb05afb308ff3ee672740b53
Guglielmo <guglielmo.celata@gmail.com>
2012-11-27 09:30:04 +0100

    citygov and council tables are now sortable, using the DataTable jquery plugin

--- open_municipio/idioticon/templatetags/popover_info.py
+++ open_municipio/idioticon/templatetags/popover_info.py
@@ -6,19 +6,21 @@ from open_municipio.idioticon.models import Term
 register = template.Library()
 
 @register.inclusion_tag('idioticon/popover_enabled_icon.html')
-def popover_info(term_slug, popover_placement='right'):
+def popover_info(term_slug, popover_placement='right', width='200px'):
     try:
         term = Term.objects.get(slug=term_slug)
     except ObjectDoesNotExist:
         return {
             'title': term_slug,
             'content': _("<em>Term not yet defined.</em>"),
-            'placement': popover_placement
+            'placement': popover_placement,
+            'width': width
         }
 
 
     return {
         'title': term.popover_title if term.popover_title is not None else term.term,
         'content': term.definition,
-        'placement': popover_placement
+        'placement': popover_placement,
+        'width': width
     }
