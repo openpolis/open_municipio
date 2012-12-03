9a6d58d0c8f545013447955e9438b4cd20d57d37
Guglielmo <guglielmo.celata@gmail.com>
2012-12-03 22:28:49 +0100

    FileImport model added to keep track of data imports

--- open_municipio/data_import/admin.py
+++ open_municipio/data_import/admin.py
@@ -1,1 +1,12 @@
 __author__ = 'guglielmo'
+
+from django.contrib import admin
+from open_municipio.data_import.models import *
+
+class FileImportAdmin(admin.ModelAdmin):
+    list_filter = ('import_type', 'import_started_at')
+    search_fields = ('file_path',)
+
+
+admin.site.register(FileImport, FileImportAdmin)
+
