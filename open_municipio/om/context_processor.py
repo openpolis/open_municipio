2710080b15f421a08b9c7d156b21aa910fe670e6
Guglielmo <guglielmo.celata@gmail.com>
2012-12-07 10:24:03 +0100

    monitoring link when not authenticated leads to login, with redirect

--- open_municipio/om/context_processor.py
+++ open_municipio/om/context_processor.py
@@ -1,9 +1,15 @@
 from django.conf import settings
+from django.utils.http import urlquote
+from django.contrib.auth import REDIRECT_FIELD_NAME
 
 def defaults(request):
     '''
     A context processor to add the default site settings to the current Context
     '''
+
+    path = urlquote(request.get_full_path())
+    url = '%s?%s=%s' % (settings.LOGIN_URL, REDIRECT_FIELD_NAME, path)
+
     return {
         'main_city': settings.SITE_INFO['main_city'],
         'beta': settings.SITE_INFO['site_version'],
@@ -11,4 +17,5 @@ def defaults(request):
         'TEMPLATE_DEBUG': settings.TEMPLATE_DEBUG,
         'IS_DEMO': settings.IS_DEMO,
         'GOOGLE_ANALYTICS': settings.WEB_SERVICES['google_analytics'],
+        'login_url': url,
     }
