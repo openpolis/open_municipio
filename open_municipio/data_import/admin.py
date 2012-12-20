6e5c04b9c510ef357c777083ca6f84fe3e7add9b
Francesco Spegni <francesco.spegni@gmail.com>
2012-12-20 16:04:26 +0100

    added support for provider in lookup of objects

--- open_municipio/data_import/admin.py
+++ open_municipio/data_import/admin.py
@@ -1,10 +1,12 @@
 from open_municipio.data_import.models import LookupPerson, \
-    LookupInstitutionCharge, LookupAdministrationCharge, LookupCompanyCharge
+    LookupInstitutionCharge, LookupAdministrationCharge, LookupCompanyCharge, \
+    Provider
 
 from django.contrib import admin 
 
-admin.site.register(LookupPerson);
-admin.site.register(LookupInstitutionCharge);
-admin.site.register(LookupAdministrationCharge);
-admin.site.register(LookupCompanyCharge);
+admin.site.register(Provider)
+admin.site.register(LookupPerson)
+admin.site.register(LookupInstitutionCharge)
+admin.site.register(LookupAdministrationCharge)
+admin.site.register(LookupCompanyCharge)
 
