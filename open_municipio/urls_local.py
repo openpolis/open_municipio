39f8f4c8da04e17599d6d3768fef7eac2c3ed91e
Guglielmo <guglielmo.celata@gmail.com>
2013-03-15 18:16:05 +0100

    unused settings and urls removed; media url changed in urls local

--- open_municipio/urls_local.py
+++ open_municipio/urls_local.py
@@ -8,6 +8,6 @@ from django.conf import settings
 
 
 urlpatterns += patterns('',
-    (r'^uploads/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
