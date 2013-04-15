ebcc3bfbf366112e10018681419e5d6ec62fc2a1
Guglielmo <guglielmo.celata@gmail.com>
2013-04-15 17:35:18 +0200

    ADMIN_MEDIA_PREFIX removed as it's deprecated

--- open_municipio/settings.py
+++ open_municipio/settings.py
@@ -297,9 +297,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'sitestatic')
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 
