2833ef25a83ad2eef864b389798fef24a6a59f41
Guglielmo <guglielmo.celata@gmail.com>
2013-04-23 11:50:11 +0200

    sitting extends now TimeStampedModel, it's needed to keep track of new sittings, as imported; the task to check for newly imported items has been renamed to new_items_alert

--- open_municipio/data_import/management/commands/new_items_alert.py
+++ open_municipio/data_import/management/commands/new_items_alert.py
@@ -6,7 +6,9 @@ from django.contrib.auth.models import User
 from django.core.management import BaseCommand
 from open_municipio.acts.models import Act
 from datetime import datetime, timedelta
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import EmailMultiAlternatives, mail_managers
+from open_municipio.people.models import Sitting
+from open_municipio.votations.models import Votation
 
 
 class Command(BaseCommand):
@@ -27,11 +29,11 @@ class Command(BaseCommand):
 
         site_domain = Site.objects.get_current().domain
 
-        yesterday = datetime.now() - timedelta(days=days)
+        time_period = datetime.now() - timedelta(days=days)
+        acts = Act.objects.filter(created__gt=time_period)
+        sittings = Sitting.objects.filter(created__gt=time_period)
 
-        latest_act = Act.objects.filter(created__gt=yesterday)
-
-        if not latest_act:
+        if not acts and not sittings:
             return
 
         # initialize email dynamic fields
@@ -45,25 +47,39 @@ class Command(BaseCommand):
             self.stdout.write('No receivers found\n')
             return
 
-        self.stdout.write('Send a digest with latest {0} Act imported to {1} receivers\n'.format(
-            len(latest_act), len(receivers)
+        self.stdout.write('Send a digest with latest {0} Acts and {1} Sittings imported to {2} receivers\n'.format(
+            len(acts), len(sittings), len(receivers)
         ))
 
-        from_email = settings.DEFAULT_FROM_EMAIL
-        subject = "OpenMunicipio - {0} : Lista degli atti importati".format(
+        from_email = settings.SERVER_EMAIL
+        subject = "[OpenMunicipio - {0}] : Lista nuovi atti e sedute".format(
             settings.SITE_INFO['main_city']
         )
 
         # compose text and html message
         message_html = "<ul>"
         text_content = ""
-        for act in latest_act:
+        for act in acts:
             url = "{0}/{1}".format(site_domain.rstrip('/'), act.get_absolute_url().lstrip('/'))
             message_html += '\n<li><a href="{0}">{1}</a>'.format(url, act)
             text_content += '\n{0}: {1}'.format(act.get_absolute_url(), act)
         message_html += '\n</ul>'
 
-        # prepare email
+        text_content += '\n'
+        message_html += '\n<ul>'
+        for sitting in sittings:
+            message_html += '\n<li><dl><dt>{0}</dt>'.format(sitting)
+            text_content += '\n{0}'.format(sitting)
+            for votation in sitting.votation_set.all():
+                url = "{0}/{1}".format(site_domain.rstrip('/'), votation.get_absolute_url().lstrip('/'))
+                message_html += '\n<dd><a href="{0}">{1}</a></dd>'.format(url, votation)
+                text_content += '\n\t{0}: {1}'.format(url, votation)
+        message_html += '\n</dl></li></ul>'
+
+        # send email to managers
+        mail_managers(
+            subject, text_content, html_message=message_html
+        )
         msg = EmailMultiAlternatives(subject, text_content, from_email, receivers)
         msg.attach_alternative(message_html, "text/html")
 
