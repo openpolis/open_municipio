b13f10d229f62f41d0531c2c201b41bf7a1fdc9a
Francesco Spegni <francesco.spegni@gmail.com>
2013-05-23 19:19:16 +0200

    refined taxonomy admin view
    now it is possible to associate tags to a category, and categories to a tag

--- open_municipio/taxonomy/admin.py
+++ open_municipio/taxonomy/admin.py
@@ -3,12 +3,17 @@ from django.utils.translation import ugettext_lazy as _
 from open_municipio.taxonomy.models import Category, Tag
 
 
+class TagInCategoryInline(admin.TabularInline):
+    model = Category.tag_set.through
+
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'count')
 
+    inlines = [ TagInCategoryInline, ]
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'count')
-    fields = ('name', 'slug')
+    fields = ('name', 'slug', 'tag_set', )
     prepopulated_fields = {"slug": ("name",)}
 
 admin.site.register(Category, CategoryAdmin)
