5dc8d6f2d4c84edd354113a92d7e26e1b5f0b518
Marco Scaloni <scalonim@gmail.com>
2014-04-10 00:14:56 +0200

    see #525

--- open_municipio/people/views.py
+++ open_municipio/people/views.py
@@ -278,10 +278,12 @@ class PoliticianDetailView(DetailView):
         # Add in a QuerySet of all the institutions
         context['institution_list'] = Institution.objects.all()
 
-        context['resources'] = dict(
-            (r['resource_type'], {'value': r['value'], 'description': r['description']})
-            for r in self.object.resources.values('resource_type', 'value', 'description')
-        )
+        context['resources'] = { }
+
+        for r in self.object.resources.values('resource_type', 'value', 'description'):
+            context['resources'].setdefault(r['resource_type'], []).append(
+                {'value': r['value'], 'description': r['description']}
+            )
 
         current_charges = self.object.get_current_institution_charges().exclude(
             institutionresponsability__charge_type__in=(
