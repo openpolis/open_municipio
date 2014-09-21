03151482b43110da3f58fd34dcaa4ffa8cb49112
Francesco Spegni <francesco.spegni@gmail.com>
2014-09-21 22:56:38 +0200

    add support for slugs in urls
    see #538

--- open_municipio/votations/urls.py
+++ open_municipio/votations/urls.py
@@ -5,7 +5,9 @@ from open_municipio.votations.views import VotationDetailView, VotationSearchVie
 urlpatterns = patterns('',
     # faceted navigation
     url(r'^$', VotationSearchView(template='votations/votation_search.html'), name='om_votation_search'),
+    # votation detail (old url, pk)
+
+    url(r'^(?P<pk>\d+)/$', VotationDetailView.as_view(), name='om_votation_detail'),
     # votation detail
-#    url(r'^(?P<pk>\d+)/$', VotationDetailView.as_view(), name='om_votation_detail'),
     url(r'^(?P<slug>[\w\-]+)/$', VotationDetailView.as_view(), name='om_votation_detail'),
 )
