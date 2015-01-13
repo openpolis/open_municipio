0e4d49cbd980b5b6fa0dd2c7dd8a74322e483de3
scaloni <scalonim@gmail.com>
2015-01-13 08:33:35 +0100

    added some fields to admin page for monitorings

--- open_municipio/monitoring/admin.py
+++ open_municipio/monitoring/admin.py
@@ -3,7 +3,7 @@ from open_municipio.monitoring.models import *
 
 
 class MonitoringAdmin(admin.ModelAdmin):
-    pass
+    list_display = [ 'user', 'content_type', 'object_pk', 'content_object', 'created_at' ]
 
 
 admin.site.register(Monitoring, MonitoringAdmin)
