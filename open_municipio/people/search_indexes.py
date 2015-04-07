46db569baad509988b315ce337bb838d74107519
scaloni <scalonim@gmail.com>
2015-04-07 23:56:11 +0200

    see #583

---
+++ open_municipio/people/search_indexes.py
@@ -0,0 +1,25 @@
+from django.utils.translation import activate
+from haystack import indexes
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+from open_municipio.people.models import Institution, InstitutionCharge, Person, SittingItem
+
+class InstitutionChargeIndex(indexes.SearchIndex, indexes.Indexable):
+
+    text = indexes.CharField(document=True, use_template=True)
+
+    person_name = indexes.CharField(model_attr='person__full_name')
+    institution = indexes.FacetCharField(model_attr='institution__lowername')
+
+    start_date = indexes.FacetDateField(model_attr='start_date')
+    end_date = indexes.FacetDateField(model_attr='end_date')
+
+    n_rebel_votations = indexes.IntegerField(indexed=False, stored=True, model_attr='n_rebel_votations')
+    n_present_votations = indexes.IntegerField(indexed=False, stored=True, model_attr='n_present_votations')
+    n_absent_votations = indexes.IntegerField(indexed=False, stored=True, model_attr='n_absent_votations')
+    n_present_attendances = indexes.IntegerField(indexed=False, stored=True, model_attr='n_present_attendances')
+    n_absent_attendances = indexes.IntegerField(indexed=False, stored=True, model_attr='n_absent_attendances')
+
+    def get_model(self):
+        return InstitutionCharge
