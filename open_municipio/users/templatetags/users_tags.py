689c771c4b832ac2bf0d073765f3ab664715a335
Francesco Spegni <francesco.spegni@gmail.com>
2015-05-03 20:46:39 +0200

    better looking comments

---
+++ open_municipio/users/templatetags/users_tags.py
@@ -0,0 +1,31 @@
+from django import template
+from django.conf import settings
+from django.contrib.auth.models import User
+from open_municipio.users.models import UserProfile
+
+register = template.Library()
+
+@register.filter
+def get_profile_image(obj):
+
+    profile = None
+    person = None
+
+    if isinstance(obj, User):
+        profile = obj.userprofile
+    elif isinstance(obj, UserProfile):
+        profile = obj
+    else:
+        raise ValueError("Unable to get a profile image from '%s'" % obj)
+
+    person = profile.person
+
+    img = None
+    if person and person.image:
+        img = person.image
+    elif profile.image:
+        img = profile.image
+    
+#    print "found: %s" % img
+
+    return img
