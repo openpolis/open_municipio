38fc8d8b18bfce6590384f7da775959e52d9c5cd
Francesco Spegni <francesco.spegni@gmail.com>
2015-11-02 23:40:57 +0100

    refactor templates to move most of alert popup code to base.html

--- open_municipio/settings.py
+++ open_municipio/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'voting',
     'haystack',
     'rest_framework',
+    'cookielaw',
     'open_municipio.events',
     'open_municipio.inline_edit',
     'open_municipio.autocomplete',
